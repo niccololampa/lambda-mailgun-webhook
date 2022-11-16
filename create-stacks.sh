1de54c5e20ceeeb685840d5a9b0990e3f994039a
niccololampa <niccololampa@gmail.com>
2022-11-16 19:27:01 +0800

    Update create-stacks sh to take input from user for credentials and email

--- create-stacks.sh
+++ create-stacks.sh
@@ -1,40 +1,29 @@
 #!/bin/bash
 
-# *** Change this to the desired name of the Cloudformation stack of 
-# your Pipeline (*not* the stack name of your app)
+# NOTE: BEFORE RUNNING YARN and AWS-CLI MUST BE INSTALLED IN YOUR SYSTEM
+
+# Change these variables if you need to
 AWS_REGION="ap-southeast-1"
-CLOUDFORMATION_S3_STACK_NAME="mailgunwebhook-s3"
+CLOUDFORMATION_S3_STACK_NAME="mailgunwebhook-s3-y"
 CLOUDFORMATION_MAIN_STACK_NAME="mailgunwebhook-main"
 S3_BUCKET_NAME="mailgunwebhookbucket"
 S3_Key="mailgunwebhooklambda.zip"
 
 
-if [ -z ${1} ]
-then
-	echo "STACK CREATION FAILED!- No Github Token"
-        echo "Pass your Github token as the first argument"
-	exit 1
-fi
-
-if [ -z ${2} ]
-then
-	echo "STACK CREATION FAILED!- No Mailgun Signing Key"
-        echo "Pass Mailgun webhook signing key as the second argument"
-	exit 1
-fi
-
-if [ -z ${3} ]
-then
-	echo "STACK CREATION FAILED!- No SNS subscription email"
-        echo "Pass email for SNS subscription signing key as the third argument"
-	exit 1
-fi
+# chose input than arguments for security purposes
+read -s -p "Enter Github Auth Token: " GITHUB_TOKEN
+echo
+read -s -p "Enter Mailgun Webhook Signing Key: " MAILGUN_KEY
+echo
+read -p "Enter email for AWS SNS Subscription: " SNS_EMAIL
+echo
 
 set -eu
 
 echo "Creating CloudFormation Stack for S3 Bucket lambda zip file"
 aws cloudformation create-stack \
         --region $AWS_REGION \
+        --no-cli-pager \
         --stack-name $CLOUDFORMATION_S3_STACK_NAME \
         --parameters ParameterKey=S3Bucket,ParameterValue=$S3_BUCKET_NAME \
         --template-body file://cloudformation-s3.yml
@@ -54,9 +43,10 @@ cd ..
 
 aws cloudformation create-stack \
         --region $AWS_REGION \
+        --no-cli-pager \
         --stack-name $CLOUDFORMATION_MAIN_STACK_NAME \
         --capabilities CAPABILITY_IAM \
         --template-body file://cloudformation-api-lambda-dynamodb-sns-cb.yml \
-        --parameters ParameterKey=S3Bucket,ParameterValue=$S3_BUCKET_NAME ParameterKey=GitHubToken,ParameterValue=${1} ParameterKey=MailgunSigningKey,ParameterValue=${2} ParameterKey=SNSSubscriptionEmail,ParameterValue=${3}
+        --parameters ParameterKey=S3Bucket,ParameterValue=$S3_BUCKET_NAME ParameterKey=GitHubToken,ParameterValue=$GITHUB_TOKEN ParameterKey=MailgunSigningKey,ParameterValue=$MAILGUN_KEY ParameterKey=SNSSubscriptionEmail,ParameterValue=$SNS_EMAIL
 
-
+echo "Executed Cloud Formation Create Commands"
